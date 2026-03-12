805a2b933a2c2aa4a81fd04141b09982849131f0
Olivier Massot <olivier.massot@2iopenservice.fr>
2026-03-12 16:44:55 +0100

    comment properties at fault

--- api/src/Api/Resource/Store.php
+++ api/src/Api/Resource/Store.php
@@ -23,6 +23,7 @@ use App\Api\Embedded\Store\Manager;
 use App\Api\Embedded\Store\Supplier;
 use App\Entity\Store as StoreEntity;
 use Symfony\Component\ObjectMapper\Attribute\Map;
+use Symfony\Component\ObjectMapper\Transform\MapCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
@@ -73,20 +74,21 @@ class Store
 
     public ?bool $published = null;
 
-//    #[Map(source: 'contact?.email')]
-//    public ?string $email = null;
+    #[Map(source: 'contact?.email')]
+    public ?string $email = null;
 
 //    #[Map(source: 'contact?.phone')]
 //    public ?string $phone = null;
 
     public ?\DateTimeInterface $openingDate = null;
 
-//    #[ApiProperty(genId: false)]
-//    public ?Manager $manager = null;
+    #[ApiProperty(genId: false)]
+    public ?Manager $manager = null;
 
-//    /** @var Toy[] */
-//    #[ApiProperty(readableLink: true, genId: false)]
-//    public array $toys = [];
+    /** @var Toy[] */
+    #[Map(transform: new MapCollection())]
+    #[ApiProperty(genId: false)]
+    public array $toys = [];
 
 //    /** @var Category[] */
 //    #[ApiProperty(genId: false)]
